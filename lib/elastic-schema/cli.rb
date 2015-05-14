616ab87f66b7e60f70aba7613e6ebfc2116eb56c
Leandro Camargo <leandroico@gmail.com>
2015-05-14 15:41:06 -0300

    Add some interface to ElasticSchema::CLI

--- lib/elastic-schema/cli.rb
+++ lib/elastic-schema/cli.rb
@@ -3,6 +3,19 @@ require 'optparse'
 module ElasticSchema
 
   class CLI
+
+    COMMANDS = %w(update check create drop recreate)
+
+    def self.commands
+      COMMANDS
+    end
+
+    def initialize(options)
+    end
+
+    def run!
+    end
+
   end
 
 end
